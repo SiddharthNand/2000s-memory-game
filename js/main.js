218c6bb62cd612f102b286d714a50f5363293c28
SiddharthNand <sidharthnand47@gmail.com>
2022-10-19 22:26:33 -0700

    Javascript set up for single card flipped
    all javascript flips first card on the board, but only in the console log
    doesnt physically flip yet

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 // Select Elements
-const card = document.querySelector('.album-card');
+const card  = document.querySelector('.album-card')
 
 // Functions 
-const onCardClicked = function(event) {
+const onCardClicked = function (event) {
   console.log(event.target)
+  
+  flipCard(event.target)
+  
+  setTimeout(function () {
+    flipCardBack(event.target)
+  }, 2000)
+}
+
+const flipCard = function (card) {
+  card.classList.remove('.back-face')
+}
+ 
+const flipCardBack = function () {
+  card.classList.add('.back-face')
 }
 
 // Attach event listeners
