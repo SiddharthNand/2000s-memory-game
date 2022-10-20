58652f6a2f026f439a2ed2f853ca5be8175eca8c
SiddharthNand <sidharthnand47@gmail.com>
2022-10-20 00:04:42 -0700

    match functionality and flip card back
    match stays, no matches flip back

--- js/main.js
+++ js/main.js
@@ -1,23 +1,40 @@
 // Select Elements
 const albums  = document.querySelectorAll('.album-card')
 
+let isFirstSameAlbumClicked = false
+
+let firstPairAlbum
+
 // Functions 
 const onAlbumClicked = function (event) {
-  console.log(event.target)
-  
   flipAlbum(event.target)
-  
-  setTimeout(function () {
-    flipAlbumBack(event.target)
-  }, 2000)
+if(isFirstSameAlbumClicked === true) {
+  if (event.target.textContent.trim() === firstPairAlbum.textContent.trim()) {
+    console.log('found match')
+    event.target.classList.add('matched-album')
+    firstPairAlbum.classList.add('matched-album')
+  } else {
+    setTimeout(function () {
+      flipAlbumBack(firstPairAlbum)
+    }, 2000)
+  }
+
+  isFirstSameAlbumClicked = false
+} else {
+  isFirstSameAlbumClicked = true
+  firstPairAlbum = event.target
+}
+  // console.log(isFirstAlbumClicked)
+  // isFirstAlbumClicked = true
+  // console.log(isFirstAlbumClicked)
 }
 
 const flipAlbum = function (album) {
-  album.classList.remove('.back-face')
+  album.classList.remove('back-face')
 }
  
 const flipAlbumBack = function (album) {
-  album.classList.add('.back-face')
+  album.classList.add('back-face')
 }
 
 // Attach event listeners
