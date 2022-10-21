b07f5e37aeadf78725ef1d82f7a9c0795ef7b895
SiddharthNand <sidharthnand47@gmail.com>
2022-10-21 00:27:26 -0700

    finished product for presentation
    needs a couple more things before adding to portfolio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // Elements
 const albums  = document.querySelectorAll('.albumCell')
 
-let firstAlbum 
+let firstAlbum
+
+
 
 // Functions
 const onAlbumClicked = function (event) {
@@ -10,12 +12,12 @@ const onAlbumClicked = function (event) {
 
   if (firstAlbum === undefined){
     firstAlbum = event.target
-    console.log(firstAlbum)
   } else {
     if (firstAlbum.dataset.id != event.target.dataset.id) {
     setTimeout(function () {
       flipAlbumBack(event.target)
       flipAlbumBack(firstAlbum)
+
       firstAlbum = undefined
       }, 1000)
       }else if (firstAlbum.dataset.id === event.target.dataset.id) {
@@ -33,10 +35,13 @@ const flipAlbumBack = function (album) {
 }
 
 
+
 // Event Listeners
 albums.forEach(function (album) {
   album.addEventListener('click', onAlbumClicked)
 })
+
+
 
 
 
